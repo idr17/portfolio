d33da3a19bed56e86ab1619970acea2e9eb5a8e9
indra <indra@mggsoftware.com>
2018-12-04 10:28:15 +0700

    alive

---
+++ src/layouts/MyLayout.vue
@@ -0,0 +1,119 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <!-- <q-layout-header>
+      <q-toolbar
+        color="red"
+        :glossy="$q.theme === 'mat'"
+        :inverted="$q.theme === 'ios'"
+      >
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          aria-label="Menu"
+        >
+          <q-icon name="menu" />
+        </q-btn>
+
+        <q-toolbar-title>
+          Portfolio 1.0
+          <div slot="subtitle">Running on Quasar v{{ $q.version }}</div>
+          <div slot="subtitle">Sekelumit tentang</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-layout-header> -->
+
+    <q-layout-drawer
+      v-model="leftDrawerOpen"
+      :content-class="$q.theme === 'mat' ? 'bg-grey-3' : null"
+      :content-style="{color: '#333'}"
+      >
+      <q-list
+        no-border
+        link
+        inset-delimiter class="listMenu"
+      >
+        <q-item>
+          <q-item-main style="text-align: center;">
+            <q-item-tile style="display: block;"><img alt="Quasar logo" src="~assets/rsz_me_and_my_lovely.jpg" style="border-radius: 100px; height: 150px;"></q-item-tile>
+            <q-item-tile style="display: block; font-weight: bold;">{{ creator.username }}</q-item-tile>
+            <q-item-tile style="display: block;">{{ creator.progies }}</q-item-tile>
+          </q-item-main>
+        </q-item>
+        <q-item />
+        <q-item>
+          <a name="about" href="#workingEksperience" class="links">
+          <!-- <q-item-side icon="school" /> -->
+          <q-item-main label="Jobs" sublabel="Working Experience" />
+          </a>
+        </q-item>
+        <q-item>
+          <a name="about" href="#portfolio" class="links">
+          <!-- <q-item-side icon="school" /> -->
+          <q-item-main label="Portfolio" sublabel="Last and Current Projects" />
+          </a>
+        </q-item>
+        <q-item>
+          <a name="about" href="#about" class="links">
+          <!-- <q-item-side icon="school" /> -->
+          <q-item-main label="About" sublabel="Personal Information & Contact" />
+          </a>
+        </q-item>
+      </q-list>
+    </q-layout-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+    <q-layout-footer class="no-shadow">
+      <q-toolbar color="grey">
+                <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          aria-label="Menu"
+        >
+          <q-icon name="menu" />
+        </q-btn>
+        <q-toolbar-title>
+          <p class="q-caption q-ma-none">By: {{ creator.nickname }}</p>
+          <div slot="subtitle">call us @{{ creator.phone }} | @github:{{ creator.github }} | @email:{{ creator.email }}</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-layout-footer>
+  </q-layout>
+</template>
+
+<script>
+import { openURL } from 'quasar'
+
+export default {
+  name: 'MyLayout',
+  data () {
+    return {
+      leftDrawerOpen: this.$q.platform.is.desktop,
+      creator: {
+        username: 'Indro Allezz',
+        nickname: 'Indro',
+        email: 'indera.allezz@gmail.com',
+        github: 'idr17',
+        phone: '081296171727',
+        progies: 'Fullstack Web Developer'
+      }
+    }
+  },
+  methods: {
+    openURL
+  }
+}
+</script>
+
+<style scoped>
+/* .parentListMenu { border: solid 1px red !important; } */
+/* .parentListMenu { color: #fff !important; background-image: url('../assets/blue-hexagon.jpeg') !important; background-repeat: repeat-y !important; } */
+/* .listMenu { color: #fff; background-image: url('~assets/blue-hexagon.jpeg'); background-repeat: repeat-y; } */
+/* .q-item-sublabel { color: #fff !important; } */
+a.links { text-decoration: none; margin-left: 40px; color: #333; }
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,231 @@
+<template>
+  <q-page>
+    <q-list class="careerContent" id="workingEksperience">
+      <q-list-header>Working Experience</q-list-header>
+      <q-item>
+        <q-item-main>
+          <q-timeline responsive color="secondary" style="padding: 0 24px;">
+            <!-- <q-timeline-entry heading>Working Experience</q-timeline-entry> -->
+            <q-timeline-entry
+              title="MGG Software Pte.Ltd"
+              subtitle="1 February 2018 - Current"
+              side="left"
+            >
+              <div>
+                Currently developing snaapp (School Notification and Attendance App).
+              </div>
+            </q-timeline-entry>
+            <q-timeline-entry
+              title="KapanLagi Network"
+              subtitle="April 2015"
+              side="right"
+            >
+              <div>
+                Write “clean”, well designed PHP Framework code, HTML, CSS, jQuery and AJAX. Produce detailed specifications. Troubleshoot, test and maintain the core product software and databases to ensure strong optimization and functionality. Contribute in all phases of the development lifecycle. Follow industry best practices. Develop and deploy new features to facilitate related procedures and tools if necessary. Implement staging development environment product.
+              </div>
+            </q-timeline-entry>
+            <q-timeline-entry
+              title="PT Omron Manufacturing of Indonesia"
+              subtitle="20 September 2010"
+              side="left"
+            >
+              <div>
+                Production Maintenance Production Switch.<br /> Responsibilities to maintain and improve machine performance. include Programming Omron PLC. Establish production maintenance system whether emergency or preventive maintenance. Making schedule of maintenance activity. Coordinate with production side to implement maintenance activity & improvement. Controlling implementation of maintenance system. Controlling machine performance. Skillup subordinates. Ensure "Safety First" in all activity & machinery. Make downtime machinery report.
+              </div>
+            </q-timeline-entry>
+          </q-timeline>
+        </q-item-main>
+      </q-item>
+      <q-item id="portfolio">
+        <q-item-main>
+          <q-list-header>
+            Portfolio
+          </q-list-header>
+        </q-item-main>
+      </q-item>
+      <q-item>
+        <q-item-main>
+          Here is some of them..
+        </q-item-main>
+      </q-item>
+      <q-item>
+        <q-item-main>
+          <q-card inline class="cardProject">
+            <q-card-media>
+              <img src="~assets/klsingapore.jpg">
+            </q-card-media>
+            <q-card-title>
+              KapanlagiKeSingapura
+              <q-rating slot="subtitle" v-model="content.project.one" :max="5" />
+              <div slot="right" class="row items-center">
+                <!-- <q-icon name="place" /> 250 ft -->
+              </div>
+            </q-card-title>
+            <q-card-main>
+              <p>Singapore Tourism Board</p>
+              <p class="text-faded">Build with Php CI, Html, Css, jQuery</p>
+            </q-card-main>
+            <q-card-separator />
+            <q-card-actions>
+              <q-btn flat round dense icon="event" />
+              <q-btn flat label="Copyright 2018" />
+            </q-card-actions>
+          </q-card>
+          <q-card inline class="cardProject">
+            <q-card-media>
+              <img src="~assets/garnier.jpg">
+            </q-card-media>
+            <q-card-title>
+              #GarnierSuperready
+              <q-rating slot="subtitle" v-model="content.project.two" :max="5" />
+              <div slot="right" class="row items-center">
+                <!-- <q-icon name="place" /> 250 ft -->
+              </div>
+            </q-card-title>
+            <q-card-main>
+              <p>Garnier Commercial Campaign</p>
+              <p class="text-faded">Build with Php CI, Html, Css, jQuery, Instagram API</p>
+            </q-card-main>
+            <q-card-separator />
+            <q-card-actions>
+              <q-btn flat round dense icon="event" />
+              <q-btn flat label="Copyright 2017" />
+            </q-card-actions>
+          </q-card>
+          <q-card inline class="cardProject">
+            <q-card-media>
+              <img src="~assets/pixy.jpg">
+            </q-card-media>
+            <q-card-title>
+              Pixy Asian Beauty
+              <q-rating slot="subtitle" v-model="content.project.three" :max="5" />
+              <div slot="right" class="row items-center">
+                <!-- <q-icon name="place" /> 250 ft -->
+              </div>
+            </q-card-title>
+            <q-card-main>
+              <p>Pixy Commercial Campaign</p>
+              <p class="text-faded">Build with Php CI, Html, Css, jQuery, Facebook Auth, Twitter Auth</p>
+            </q-card-main>
+            <q-card-separator />
+            <q-card-actions>
+              <q-btn flat round dense icon="event" />
+              <q-btn flat label="Copyright 2018" />
+            </q-card-actions>
+          </q-card>
+          <q-card inline class="cardProject">
+            <q-card-media>
+              <img src="~assets/snaappapp.jpg">
+            </q-card-media>
+            <q-card-title>
+              SNAAPP
+              <q-rating slot="subtitle" v-model="content.project.one" :max="5" />
+              <div slot="right" class="row items-center">
+                <!-- <q-icon name="place" /> 250 ft -->
+              </div>
+            </q-card-title>
+            <q-card-main>
+              <p>School Notification and Attendance App</p>
+              <p class="text-faded">Build with NodeJs, VueJs+Quasar Framework, AngularJs*</p>
+            </q-card-main>
+            <q-card-separator />
+            <q-card-actions>
+              <q-btn flat round dense icon="event" />
+              <q-btn flat label="Copyright 2018" />
+            </q-card-actions>
+          </q-card>
+        </q-item-main>
+      </q-item>
+    </q-list>
+    <q-list class="personalContent">
+      <div id="about">
+      <!-- <q-list-header>About</q-list-header> -->
+      <q-item>
+        <q-item-main>
+          {{ content.aboutPage }}<br>
+          Semoga saya tidak salah =))
+        </q-item-main>
+      </q-item>
+      <q-list-header>
+        Basic Personal Information
+      </q-list-header>
+      <q-item v-for="(val, k) in content.basicInfo" :key="k">
+        <q-item-side>{{ k | capitalize }}</q-item-side><q-item-side />
+        <q-item-main>{{ val | capitalize }}</q-item-main>
+      </q-item>
+      <q-list-header>
+        Education
+      </q-list-header>
+      <q-item v-for="(val, k) in content.education" :key="k">
+        <q-item-side>{{ k | capitalize }}</q-item-side><q-item-side />
+        <q-item-main>{{ val | capitalize }}</q-item-main>
+      </q-item>
+      <q-item>
+        <q-list-header>
+          Technical Skill
+        </q-list-header>
+      </q-item>
+      <q-item v-for="(v, k) in content.skills" :key="k">
+          {{ k }}
+        <q-item-main class="skillbar">
+          <q-progress :percentage="v.val" :color="v.color" />
+        </q-item-main>
+      </q-item>
+      <q-item><q-item-main>&nbsp;</q-item-main></q-item>
+    </div>
+    </q-list>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+      content: {
+        aboutPage: 'Hi, saya biasa dipanggil indro. Saya suka belajar & fastlearner yang terobsesi dengan software development.',
+        basicInfo: {
+          name: 'Indra Kurneamin',
+          age: '29 Years Old',
+          hobby: 'Coding, Football, Coffee'
+        },
+        education: {
+          diploma: 'Manajemen Informatika - Politeknik Negeri Malang',
+          bachelor: 'Fasilkom - Universitas Mercubuana'
+        },
+        project: {
+          one: 4,
+          two: 3,
+          three: 5
+        },
+        skills: {
+          PHP_CodeIgniter: { val: 80, color: 'primary' },
+          YII: { val: 50, color: 'yellow' },
+          CSS: { val: 70, color: 'secondary' },
+          Node_FeathersJs: { val: 75, color: 'info' },
+          VueJs: { val: 75, color: 'green' },
+          QuasarFW: { val: 75, color: 'warning' },
+          AngularJs: { val: 85, color: 'warning' },
+          GIT: { val: 75, color: 'red' }
+        }
+      }
+    }
+  },
+  filters: {
+    capitalize: function (value) {
+      if (!value) return ''
+      value = value.toString()
+      return value.charAt(0).toUpperCase() + value.slice(1)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.q-list { border: none; }
+.personalContent { padding: 50px 30px; }
+.careerContent { padding: 0 30px; }
+.q-list-header { font-size: 30px; margin: 30px 0 20px 0;  }
+.skillbar { margin-left: 10px; }
+.cardProject { width: 290px; margin: 0 10px 0 0; }
+</style>
